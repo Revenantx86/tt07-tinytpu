fce8ef370310a89e9756b76b4a8110d51baa488b
Revenantx86 <69749268+Revenantx86@users.noreply.github.com>
2024-05-14 11:23:32 -0400

    updated test

--- test/test.py
+++ test/test.py
@@ -4,14 +4,27 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
+# Extra random library
+from cocotb.triggers import RisingEdge, Timer
+from cocotb.clock import Clock
+import random
 
+# Parameters
+TEST_DURATION = 1000  # Test duration in simulation time units
+D_W = 8
+N = 2
+
+  # data_in_x = ui_in[0];
+  # data_in_y = ui_in[1];
+  # load_en   = ui_in[2];
+  # init      = ui_in[3];
 
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    clock = Clock(dut.clk, 40, units="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -20,17 +33,60 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 40)
     dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
+
 
     # Set the input values you want to test
     #dut.ui_in.value = 20
     #dut.uio_in.value = 30
 
+    #Load Data
+    dut.ui_in[0] <= 1; # Enable load
+    for i in range(32):
+        dut.ui_in[0] <= random.randint(0, 1)  # data_in_x
+        dut.ui_in[1] <= random.randint(0, 1)  # data_in_y
+        await Timer(40, units="ns")
+
+    dut.ui_in[2] <= 0  # De assert load
+    await Timer(120, units="ns")  # Waiting period
+
+    # Initialize
+    dut.ui_in[3] <= 1  # init
+    await Timer(40, units="ns")
+    dut.ui_in[3] <= 0  # init
+
+    bit_counter = 0
+    index_counter = 0
+    data = 0
+    cc11 = 0
+    cc12 = 0
+    cc21 = 0
+    cc22 = 0
+
+    for _ in range(N*N*16):
+        await RisingEdge(dut.clk)
+        bit_counter += 1
+        data = (int(dut.uo_out[0]) << (2 * D_W - 1)) | (data >> 1)
+        
+        if index_counter == 0 and bit_counter == (2 * D_W) - 1:
+            cc11 = data
+        elif index_counter == 1 and bit_counter == (2 * D_W) - 1:
+            cc12 = data
+        elif index_counter == 2 and bit_counter == (2 * D_W) - 1:
+            cc21 = data
+        elif index_counter == 3 and bit_counter == (2 * D_W) - 1:
+            cc22 = data
+
+        if bit_counter == (2 * D_W) - 1:
+            index_counter += 1
+
+    await Timer(TEST_DURATION, units="ns")  # End simulation after a set duration
+    
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    #await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
