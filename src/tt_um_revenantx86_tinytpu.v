edf5274f6e2274c14eb27cf0c77dfbec1f86c98f
Revenantx86 <69749268+Revenantx86@users.noreply.github.com>
2024-05-13 20:41:11 -0400

    output io assigned

--- src/tt_um_revenantx86_tinytpu.v
+++ src/tt_um_revenantx86_tinytpu.v
@@ -47,6 +47,7 @@ module tt_um_revenantx86_tinytpu
   assign uo_out[7] = 0;
 
   assign uio_oe = 8'b00;
+  assign uio_out = 8'b0;
 
   tinytpu_top #(.D_W(8), .N(2), .WORD(4)) 
       tinytpu_top_inst (
