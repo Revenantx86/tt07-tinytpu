253ae88b4073909891196abb7699f701c9251e01
Revenantx86 <69749268+Revenantx86@users.noreply.github.com>
2024-05-13 20:01:49 -0400

    uh

--- src/tt_um_revenantx86_tinytpu.v
+++ src/tt_um_revenantx86_tinytpu.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_tinytpu_top 
+module tt_um_revenantx86_tinytpu 
 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
